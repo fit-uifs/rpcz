ca9a490254511b1ca1845b1a609f0b6a7d17cabe
goready <goready@163.com>
2017-04-11 13:44:23 +0800

    linux ok

--- cmake-build-debug/examples/cpp/cmake_install.cmake
+++ cmake-build-debug/examples/cpp/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /media/psf/Home/Documents/work-ubuntu/rpcz-master-linux-ok/examples/cpp
+# Install script for directory: /media/psf/Home/Documents/work-ubuntu/rpcz/examples/cpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
